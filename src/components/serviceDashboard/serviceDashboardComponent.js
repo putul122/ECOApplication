7950b08485ed711af7b710feffae15f5c7094435
Putul Sahani <putul.sahani@neosofttech.com>
2019-03-11 11:15:38 +0530

    resolved searvice dashboard count issues

--- src/components/serviceDashboard/serviceDashboardComponent.js
+++ src/components/serviceDashboard/serviceDashboardComponent.js
@@ -20,9 +20,12 @@ export default function ServiceDashboard (props) {
       }
       console.log('List', List)
       parts.forEach(function (partData, i) {
-        if (partData.value.metric === 'count') {
-          subjectCount = partData.value.value
+        if (partData.value.hasOwnProperty(subjectName)) {
+          subjectCount = partData.value[subjectName].count
         }
+        // if (partData.value.metric === 'count') {
+        //   subjectCount = partData.value.value
+        // }
       })
       return (
         <div className='col-md-4'>
