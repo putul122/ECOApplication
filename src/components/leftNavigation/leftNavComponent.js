480e7cba1ded66a4371446b9e6dffe70e6c253ce
Putul Sahani <putul.sahani@neosofttech.com>
2019-05-02 13:51:37 +0530

    added checks for selected package in left navigation

--- src/components/leftNavigation/leftNavComponent.js
+++ src/components/leftNavigation/leftNavComponent.js
@@ -4,6 +4,7 @@ import styles from './leftNavComponent.scss'
 
 class LeftNavigation extends React.Component {
 	render () {
+    const selectedPackage = JSON.parse(localStorage.getItem('selectedPackage'))
 		return (
   <div>
     {/* <!-- BEGIN: Left Aside --> */}
@@ -35,7 +36,7 @@ class LeftNavigation extends React.Component {
           </div>
           <ul id='' className={'m-menu__item m-menu__item--submenu'}>
             <li className={'m-menu__item  m-menu__item--submenu m-menu__item--submenu-fullheight '} style={{'paddingTop': '20px'}}>
-              <a className='m-menu__link' href='/home'>
+              <a className='m-menu__link' href={selectedPackage && selectedPackage.key === 'ECO_SLA' ? '/sla-dashboard' : '/home'}>
                 <i className='m-menu__link-icon flaticon-menu' style={{'font-size': '25px', 'color': '#01002F'}} />
                 <span className={styles.navtext}>
                 Home
