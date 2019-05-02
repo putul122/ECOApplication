aa7cd9138b70ea672495e03d3d5dcf7f5b451943
Dhaval chheda <dhavalchheda@eross-MacBook-Pro-2.local>
2019-05-02 11:40:41 +0530

    missing pie-chart new json for Huawei

---
+++ src/components/SlaDashboard/sla-dashboard-huawei-compliance-pie-chart.js
@@ -0,0 +1,25 @@
+export const HuaweiSlaDashboardPieChartJson = [
+	{
+		'perspective_id': 15,
+		'view_key': 'Contracts_Compliance',
+		'subject_id': 0,
+		'parts': [
+			{
+				'value': [
+					{
+						'key': 'Compliant',
+						'value': {
+							'count': 28
+						}
+					},
+					{
+						'key': 'Not Compliant',
+						'value': {
+							'count': 168
+						}
+					}
+				]
+			}
+		]
+	}
+]
